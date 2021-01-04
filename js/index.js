388edc426c474cc16112477f9d84cf31dbc720f2
Beto Cortés <roberto.cortes13233@gmail.com>
2021-01-04 14:43:47 -0600

    mobile first done

--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ simplyCountdown('#count', {
   minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
   words: { //words displayed into the countdown
-    days: { singular: 'day', plural: 'days' },
-    hours: { singular: 'hour', plural: 'hours' },
-    minutes: { singular: 'minute', plural: 'minutes' },
-    seconds: { singular: 'second', plural: 'seconds' }
+    days: { singular: 'DAY', plural: 'DAYS' },
+    hours: { singular: 'HOUR', plural: 'HOURS' },
+    minutes: { singular: 'MINUTE', plural: 'MINUTES' },
+    seconds: { singular: 'SECOND', plural: 'SECONDS' }
   },
   plural: true, //use plurals
   inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-  inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+  inlineClass: 'counts', //inline css span class in case of inline = true
   // in case of inline set to false
   enableUtc: false, //Use UTC or not - default : false
   onEnd: function () { return; }, //Callback on countdown end, put your own function here
